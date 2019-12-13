2f48a1f0cc216f606b983c90007611617ced931a
Cypre55 <satwik.chappidi@iitkgp.ac.in>
2019-12-14 01:28:05 +0530

    Adds caps.py and JSONs.py, Makes paths OS-unspecific, Updates README.md This commit adds two new file, JSONs.py and caps.py. They have been added to let the user, add and edit captions as well as use multiple JSON files as the source. The README and redins.py have been adjusted accordingly.

---
+++ caps.py
@@ -0,0 +1,65 @@
+# Edit or Add Captions following the examples present below
+# For adding Instagram Captions, use Instagram_Caps()
+# For adding Twitter Captions, use Twitter_Caps()
+# Please do these below line 19
+
+
+def Instagram_Caps(string):
+    cap.append(string)
+
+
+def Twitter_Caps(string):
+    caps.append(string)
+
+
+caps = []
+
+cap = []
+
+
+Instagram_Caps("""Follow @einteresting
+        .
+        .
+        .
+        .
+        .
+        #dankmemes #memes #dank #meme #funny #funnymemes #lol
+        #edgymemes #lmao #memesdaily #dankmeme #offensivememes
+        #comedy #offensive #follow #like #funnyvideos #fortnitememes
+        #haha #oof
+        """)
+
+Instagram_Caps("""Follow @einteresting
+        .
+        .
+        .
+        .
+        #meme #memes #funny #dankmemes #dank #lol
+        #memesdaily #lmao #funnymemes #follow
+        #fortnite #like #edgy #dankmeme #edgymemes
+        #comedy #cringe #humor #funnyvideos #lmfao
+        #instagram #hilarious #oof #savage""")
+
+Instagram_Caps("""Follow @einteresting
+        .
+        .
+        .
+        .
+        .
+        #laugh #gaming #minecraft #cancer #music #f #funnymeme
+        #instagood #kpop #edgymeme #love #roblox #joke #viral #lit
+        #memepage #ifunny #lilpump #rofl #life #donaldtrump
+        """)
+Instagram_Caps("""Follow @einteresing
+        .
+        .
+        .
+        .
+        #funny #funnymeme #funnymemes #memes #meme #funnytext #funnyvideos
+        #hilarious #crazy #humor #epic #instafun
+        """)
+
+
+Twitter_Caps("Follow @E__interesting for more #funny #lol #memes #follow")
+Twitter_Caps("Follow @E__interesting for more #follow #dankmemes #reddit")
+Twitter_Caps("Follow @E__interesting for more #lmao #edgymemes #comedy #retweet")
