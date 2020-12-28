5afe0a28a119017e85e2b945fce1a7f786021da7
sudo-gyan <gyanaranjan7205@gmail.com>
2020-12-29 00:54:13 +0530

    made config.py pass flake test and added configparser as requirement

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import ast
 import logging
 logger = logging.getLogger()
 
+
 class Config:
     """
     Load data from config file
@@ -12,19 +13,20 @@ class Config:
 
         c = configparser.ConfigParser()
         c.read(file)
-        
+
         # self.modules - dict of social media websites to post
         # Eg: {'instagram':True, 'facebook':False}
-        self.modules = {a: ast.literal_eval(b)
+        self.modules = {str(a): ast.literal_eval(b)
                         for a, b in dict(c["post"]).items()}
-        
+
         # credentials for all social media profiles
         # {'instagram':{'username':'@username', 'password':'PASSWORD'}}
         self.credentials = dict()
         for module, should_post in self.modules.items():
             if should_post is True:
-                self.credentials[module] = {a: ast.literal_eval(b)
-                                      for a, b in dict(c[module]).items()}
+                self.credentials[module] = {
+                    a: ast.literal_eval(b)
+                    for a, b in dict(c[module]).items()}
 
         self.subreddits = ast.literal_eval(c['reddit']['subreddits'])
         self.sub_category = ast.literal_eval(c['reddit']['category'])
@@ -33,7 +35,8 @@ class Config:
 
         print("Subreddits Loaded: ", ",".join(self.subreddits))
         for module, should_post in self.modules.items():
-            logger.debug(module, "Loaded" if should_post is True else "Not Loaded")
+            logger.debug(
+                module, "Loaded" if should_post is True else "Not Loaded")
             if should_post is True:
                 logger.debug("Credentials:")
                 for key, value in self.credentials[module].items():
